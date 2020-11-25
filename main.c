45220ce2b142ab6865cde4ec011acf030ad8a9dd
Dmytri <74990472+dmytrish@users.noreply.github.com>
2020-11-25 21:13:47 +0200

    Add files via upload

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    float a, b, c;
+    printf("Vvedite chislo:\n");
+    scanf("%f", &a);
+    printf("Vvedite VTOROYE chislo:\n;
+    scanf("%f", &b);
+    c=(a+b)/2;
+    printf("Posdravlyayem! Srednee arifmeticheskoye — %f\n", c);
+    return 0;
+}
